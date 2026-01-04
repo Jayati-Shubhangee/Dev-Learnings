9a825ca20faae96c17c9bec6af3e35d34e584ed7
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-01-04 12:21:36 +0000

    variables in javascript

---
+++ js/01variables.js
@@ -0,0 +1,11 @@
+const accountid=1009
+let accountemail="jay.com"
+var accountpassword="1234"
+accountcity="noida"
+//accountid=12334// this is not valid not allowed
+accountemail="js.com"
+accountpassword="0011"
+accountcity="gurugram"
+let price
+console.table([accountid,accountemail,accountpassword,accountcity,price]);
+/*NOTE- NOT TO USE VAR USE LET INSTEAD AS VAR GIVES ISSUE WITH BLOCK SCOPE OR FUNCTIONAL SCOPE*/
