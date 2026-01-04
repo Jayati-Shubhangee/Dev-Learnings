ced84742223aa7d43f106527acb726c5f398a2f7
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-01-04 14:07:21 +0000

    conversion study in js

---
+++ js/03conversionsOperations.js
@@ -0,0 +1,46 @@
+/*let score1 = "45" 
+let score2 = "45.009" 
+let score3="234abc"
+let score4 = "45+45" 
+let score5 = undefined
+let score6 = null 
+console.log(typeof score1);
+console.log(typeof(score2));
+console.log(typeof(score3));
+console.log(typeof(score4));
+ let conToNumber1= Number(score1)
+console.log(typeof conToNumber1);
+let conToNumber2= Number(score2)
+console.log(typeof conToNumber2);
+let conToNumber3= Number(score3)
+console.log(typeof conToNumber3);
+let conToNumber4= Number(score4)
+console.log(typeof conToNumber4);
+let conToNumber5= Number(score5)
+console.log(typeof conToNumber5);
+let conToNumber6= Number(score6)
+console.log(typeof conToNumber6);
+console.log(conToNumber1);
+console.log(conToNumber2);
+console.log(conToNumber3);//have abc still showed number but printed NaN 
+console.log(conToNumber4);
+console.log(conToNumber5);
+console.log(conToNumber6);
+//if test for booolean value true - we will get 1 , if write string then NAN
+//NOTES
+//"45"- 45
+// 45ABC=NAN
+//FALSE=0*/
+ let ischeck = 1
+ let boolcon=Boolean(ischeck)
+ console.log(boolcon);
+//NOTE 1 WILL CONVERT TO TRUE , O TO FALSE ""= FALSE, "JOY" TRUE
+//conversion to string check 
+let num=123+12345
+let stringcheck=String(num)
+console.log(stringcheck)
+console.log(typeof stringcheck)
+//investigation check 
+
+
+
