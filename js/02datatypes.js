0c4904ce6041997c1217c48a406ba38c8ed29a22
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-01-04 12:18:31 +0000

    Add JavaScript datatypes

---
+++ js/02datatypes.js
@@ -0,0 +1,24 @@
+//TO USE NEWER VERSION OF JS WE USE THIS SAFER VERSION NOT COMPULSORY TO USE AS BY DEFAULT IT COMES IN STRICTER VERSION
+"use strict";
+//alert("hello") //NOTE WE ARE USING NODE JS HERE NOT WORKING ON BROWER 
+//AS JS USES BROWER ENGINE BUT WE ARE NOT USING HERE WE ARE USING NODEJS HERE
+
+console.log("jayati") ;console.log("sneha")//though its correct but CODE READIBILITY SHOULD BE HIGH 
+let name="hitesh"
+let age= 3
+let islogged = false
+
+//number = 2 to pwer 53 
+//bigit
+//string 
+//boolean
+//null standalone value
+//undefined varsible declared but no value assigned
+//null is something not unintentioanlly emty but we have intentioanllly defined null value to variable 
+//symbol 
+//object
+console.log(typeof "joy");
+console.log(typeof null);//it wil return an object 
+console.log(typeof undefined);
+//undefined always has a speacial type undefined not like null object
+
