f87fb7d209e7945914f00be0500156aa4e6392c2
Jayati Shubhangee <jayati.shubhangee@gmail.com>
2026-02-02 16:00:29 +0000

    datatype sumup

---
+++ js/dtsumup.js
@@ -0,0 +1,22 @@
+//two types primitive and non primitive
+//primitive
+//7 types String call by value , changes made in copy not original Number, Bool, null,undefined,symbol for making value unique, BigInt scientific values usage
+
+
+//REFERENCE TYPE () NON PRIMITVE memeory reference directly allocated 
+// ARRAYS, OBJECTS , FUNCTIONS
+
+
+//JS IS A DYNAMICALLY TYPES LANGUAGE
+
+const fruits=["apple","orange","watermelon"];
+let obj={
+    name:"jayati",
+    age:20,
+}
+
+funstion()
+const myfunc=function(){
+    console.log("hi");
+}
+
